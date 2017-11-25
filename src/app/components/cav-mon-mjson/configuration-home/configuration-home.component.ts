44779bb431dbbfe7afaa032960ab7c15b516822c
Pooja <pooja.gupta@cavisson.com>
2017-11-25 18:18:06 +0530

    json for tree table componnent -Pooja

--- src/app/components/cav-mon-mjson/configuration-home/configuration-home.component.ts
+++ src/app/components/cav-mon-mjson/configuration-home/configuration-home.component.ts
@@ -21,6 +21,7 @@ export class ConfigurationHomeComponent implements OnInit
 
   jsonName :String;
 
+  tierList: any[];
 
 
   constructor(private cavMonConfigService:CavmonConfigService,
@@ -43,13 +44,35 @@ export class ConfigurationHomeComponent implements OnInit
    let that = this;
    this.cavMonConfigService.getTierList(this.topoName).subscribe(data =>
        {
+        this.tierList = data;
         data.forEach((function(val){
          that.cols.push({field:val ,header :val})
         }
     ))
    })
 
-   this.cavMonConfigService.getTreeTableData().then(data => this.compData = data)
+   this.cavMonConfigService.getTreeTableData().then(data =>
+     {
+      console.log("data---",data)
+      data.map(function(val)
+      {
+        that.tierList.map(function(eachTier)
+        {
+          //adding Tier to parent monitor node
+          val.data.eachTier = "configure";
+
+          //adding Tier to children monitor node
+          val.children.map(function(eachChildNode)
+          {
+            eachChildNode.data.eachTier = "configure";
+          }
+         )
+        }
+       ) 
+      })
+      this.compData = data
+     } 
+    )
  /*  this.cols = [
             {field: 'monitor' ,header:'Monitor'},
             {field: 'stresshle-blue-accservice',header:'stresshle-blue-accservice'},
