0238be9fba080ae8153203e771b5abb8b08367d6
Pooja <pooja.gupta@cavisson.com>
2018-01-08 17:15:45 +0530

    css changes

---
+++ src/app/render-compNOT/render-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-render-comp',
+  templateUrl: './render-comp.component.html',
+  styleUrls: ['./render-comp.component.css']
+})
+export class RenderCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
