37257d6bb872a8b5cc6d815e58d5525fe1b54add
NishaSingh <nishasingh.ns1994@gmail.com>
2018-01-17 19:33:34 +0530

    Changes for the profile list in the home screen

--- src/app/constants/monconfig-url-constant.ts
+++ src/app/constants/monconfig-url-constant.ts
@@ -1,7 +1,7 @@
 
 // const  SERVICE_URL = 'http://10.10.40.7:8006/ProductUI/productSummary/MonitorWebService';
-// const  SERVICE_URL = 'https://10.10.50.5/ProductUI/productSummary/MonitorWebService';
-const  SERVICE_URL = 'https://10.10.60.12/ProductUI/productSummary/MonitorWebService';
+const  SERVICE_URL = 'https://10.10.50.5/ProductUI/productSummary/MonitorWebService';
+// const  SERVICE_URL = 'https://10.10.60.12/ProductUI/productSummary/MonitorWebService';
 
 //for running configUI as a standAlone
 export const ROUTING_PATH: string = "";
@@ -9,6 +9,7 @@ export const ROUTING_PATH: string = "";
 /* Url for Home Screen */
 export const GET_TOPO_LIST = `${SERVICE_URL}/getListOfTopology`;
 export const GET_TIER_LIST = `${SERVICE_URL}/getTiers`;
+export const GET_PROFILE_LIST = `${SERVICE_URL}/getProfileListFromTopo/`;
 // export const UPDATE_TOPOLOGY = `${SERVICE_URL}/uploadtopology`;
 
 
